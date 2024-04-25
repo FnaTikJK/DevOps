34a58dc29e1882e760406109641453b77e97c007
Anton Yaskov <yaskovanton333@gmail.com>
2024-04-25 21:19:53 +0400

    queue

---
+++ DevOpsAPI/DevOpsAPI/QueueService.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.Runtime;
+using Amazon.SQS;
+using DevOpsAPI.Infra;
+
+namespace DevOpsAPI;
+
+public interface IQueueService
+{
+    Task SendMessage(string message);
+}
+
+public class QueueService : IQueueService
+{
+    private readonly string queueUrl;
+    private readonly AmazonSQSClient client;
+    
+    public QueueService(BasicAWSCredentials creds)
+    {
+        var amazonConfig = new AmazonSQSConfig()
+        {
+            
+        };
+        
+        client = new AmazonSQSClient(creds);
+        queueUrl = Config.Yandex.QueueUrl;
+    }
+
+
+    public async Task SendMessage(string message)
+    {
+        await client.SendMessageAsync(queueUrl, message);
+    }
+}
